c1117c413e2f5e9f6bb9ad2b47516d74436deb40
ao627515 <ao627515@gmail.com>
2024-07-09 01:22:41 +0000

    find_by_email

--- lib/user.rb
+++ lib/user.rb
@@ -1,15 +1,19 @@
 class User
-    attr_accessor :email, :age
-  
-    @@users = []
-  
-    def initialize(email, age)
-      @email = email
-      @age = age
-      @@users.push(self)
-    end
-  public
-    def self.all
-      @@users
-    end
+  attr_accessor :email, :age
+
+  @@users = []
+
+  def initialize(email, age)
+    @email = email
+    @age = age
+    @@users.push(self)
   end
+
+  def self.all
+    @@users
+  end
+
+  def self.find_by_email(email)
+    @@users.find { |user| user.email == email }
+  end
+end
